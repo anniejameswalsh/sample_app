0cc03bfbc169ea41dbfda9c31b68784e1abce717
anniejameswalsh <anniejames@revv.com>
2021-09-30 13:26:10 -0500

    final microposts changes

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
 	before_action :correct_user,   only: :destroy
+
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
 		
@@ -16,18 +17,23 @@ class MicropostsController < ApplicationController
 
 	def destroy
 		@micropost.destroy
-		flash[:succes] = "Micropost deleted"
-		redirect_to request.referrer || root_url
+	  flash[:success] = "Micropost deleted"
+	  if request.referrer.nil? || request.referrer == microposts_url
+	    redirect_to root_url
+	  else
+	    redirect_to request.referrer
+	  end
 	end
 
 	private 
-		def micropost_params
-			params.require(:micropost).permit(:content, :image)
-		end
+	
+  def micropost_params
+		params.require(:micropost).permit(:content, :image)
+	end
 
-		def correct_user
-			@micropost = current_user.microposts.find_by(id: params[:id])
-			redirect_to root_url if @micropost.nil?
-		end
+	def correct_user
+		@micropost = current_user.microposts.find_by(id: params[:id])
+  	redirect_to root_url if @micropost.nil?
+	end
 
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class MicropostTest < ActiveSupport::TestCase
   
   def setup
-    @user = users(:annie)
-    @micropost = @user.microposts.build(content: "Lorem ipsum")
+    
+    @micropost = Micropost.new
   end
 
 
@@ -12,23 +12,6 @@ class MicropostTest < ActiveSupport::TestCase
     assert @micropost.valid?
   end
 
-  test "user id should be present" do 
-    @micropost.user_id = nil
-    assert_not @micropost.valid?
-  end
 
-  test "content  should be present" do 
-    @micropost.content = "  "
-    assert_not @micropost.valid?
-  end
-
-  test "content should be at most 140 characters" do 
-    @micropost.content = "a" * 141
-    assert_not @micropost.valid?
-  end
-
-  test "order should be most recent first" do
-    assert_equal Micropost.first, microposts(:most_recent)
-  end
 
 end
